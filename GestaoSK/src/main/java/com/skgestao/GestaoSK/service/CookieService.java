c0635ec26d747e268b0084388632146f54865114
Gleicilene <1410522@sga.pucminas.br>
2024-08-04 10:21:53 -0300

    Alteração do Cookie

--- GestaoSK/src/main/java/com/skgestao/GestaoSK/service/CookieService.java
+++ GestaoSK/src/main/java/com/skgestao/GestaoSK/service/CookieService.java
@@ -13,21 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CookieService {
 
-    public static void setCookie(HttpServletResponse response, String key, String valor, int segundos) throws IOException{
-      Cookie cookie = new Cookie(key, URLEncoder.encode(valor, "UTF-8"));
-      cookie.setMaxAge(segundos);
-      cookie.setHttpOnly(true);
-      response.addCookie(cookie);  
+    public static void setCookie(HttpServletResponse response, String key, String valor, int segundos) throws IOException {
+        Cookie cookie = new Cookie(key, URLEncoder.encode(valor, "UTF-8"));
+        cookie.setMaxAge(segundos);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
-    
-    public static String getCookie(HttpServletRequest request, String key) throws UnsupportedEncodingException{
-      String valor = Optional.ofNullable(request.getCookies())
-            .flatMap(cookies -> Arrays.stream(cookies)
-            .filter(cookie -> key.equals(cookie.getName()))
-            .findAny()).map(e -> e.getValue())
-            .orElse(null);
 
-      valor = URLDecoder.decode(valor, "UTF-8");
-      return valor;
+    public static String getCookie(HttpServletRequest request, String key) {
+        String valor = Optional.ofNullable(request.getCookies())
+                .flatMap(cookies -> Arrays.stream(cookies)
+                        .filter(cookie -> key.equals(cookie.getName()))
+                        .findAny())
+                .map(Cookie::getValue)
+                .orElse(null);
+
+        if (valor != null) {
+            try {
+                valor = URLDecoder.decode(valor, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return valor;
     }
 }
